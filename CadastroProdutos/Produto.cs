67f3a727070afc92c7ddde39a5bdba3e4f93be60
IGOR <IGOR@IGOR>
2024-03-25 17:24:49 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroProdutos/Produto.cs
@@ -0,0 +1,54 @@
+﻿
+
+
+
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Globalization;
+using System.Xml;
+namespace ProdutoDados { 
+public class Produto
+{
+    private string nome;
+    private double preco;
+    private int quantidadeEstoque;
+
+    public string Nome
+    {
+        get { return nome; }
+        set { nome = value; }
+    }
+
+    public double Preco
+    {
+        get { return preco; }
+        set { preco = value; }
+    }
+
+    public int QuantidadeEstoque
+    {
+        get { return quantidadeEstoque; }
+        set { quantidadeEstoque = value; }
+    }
+
+    public double ValorTotalEstoque
+    {
+        get { return preco * quantidadeEstoque; }
+    }
+
+    public void AdicionarProduto(int quantidade)
+    {
+        if (quantidade < 0)
+        {
+            throw new ArgumentOutOfRangeException("Quantidade não pode ser negativa.");
+        }
+
+        quantidadeEstoque += quantidade;
+    }
+
+    public override string ToString()
+    {
+        return $"Nome: {nome}\nPreço: {preco:C2}\nQuantidade em estoque: {quantidadeEstoque}\nValor total em estoque: {ValorTotalEstoque:C2}";
+    }
+}
+}
---
+++ CadastroProdutos/Programa.cs
@@ -0,0 +1,42 @@
+﻿
+// Importa o namespace ProdutoDados para ter acesso à classe Produto
+using ProdutoDados;
+
+// Diretivas using para funcionalidades comumente usadas (não estritamente necessárias aqui)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Cria um novo objeto Produto
+        Produto produto = new Produto();
+
+        // Solicita ao usuário os detalhes do produto
+        Console.WriteLine("Informe os dados do produto: ");
+
+        Console.Write("Nome do Produto: ");
+        produto.Nome = Console.ReadLine();
+
+        Console.Write("Preço do produto: ");
+        string precoStr = Console.ReadLine();
+        produto.Preco = double.Parse(precoStr);
+
+        Console.Write("Quantidade em estoque: ");
+        string quantidadeStr = Console.ReadLine();
+        produto.QuantidadeEstoque = int.Parse(quantidadeStr);
+
+        // Exibe os dados do produto
+        Console.WriteLine(produto.ToString());
+    }
+}
+
+
+
+
